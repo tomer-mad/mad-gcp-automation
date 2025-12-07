aa42f25f384454bc173bd614a52c7ffcf03e20cc
tomer-mad <tomer@madgrowth.com>
2025-12-07 18:20:12 +0200

    add source function

--- deployer-budget.sh
+++ deployer-budget.sh
@@ -3,7 +3,8 @@
 # The target project for which the budget will be enforced.
 TARGET_PROJECT_ID="mad-mmm-poc"
 
-# The budget amount in USD.
+# The budget amount, in the currency of your billing account.
+# CHANGE THIS VALUE to your desired budget amount in ILS.
 BUDGET_AMOUNT="276"
 
 # Retrieve the Billing Account ID associated with the target project.
@@ -19,12 +20,13 @@ fi
 
 echo "Submitting Cloud Build job with the following parameters:"
 echo "Target Project ID: ${TARGET_PROJECT_ID}"
-echo "Budget Amount: ${BUDGET_AMOUNT}"
+echo "Budget Amount: ${BUDGET_AMOUNT} (in billing account currency)"
 echo "Billing Account ID: ${BILLING_ACCOUNT_ID}"
 
 # Submit the Cloud Build job with all required parameters as substitutions.
 gcloud builds submit --config gcp-project-budget/deploy-cost-enforcement.yaml \
+  --logging=CLOUD_LOGGING_ONLY \
   --substitutions=_TARGET_PROJECT_ID="${TARGET_PROJECT_ID}",_BUDGET_AMOUNT="${BUDGET_AMOUNT}",_BILLING_ACCOUNT_ID="${BILLING_ACCOUNT_ID}" \
-  --service-account="projects/madgrowth-data/serviceAccounts/budget-deployer-sa@madgrowth-data.iam.gserviceaccount.com"
+  --service-account="budget-deployer-sa@madgrowth-data.iam.gserviceaccount.com"
 
 echo "Cloud Build job submitted successfully."
